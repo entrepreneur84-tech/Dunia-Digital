023cb50cc7273bafd699604097c4cef0a68cd81c
Entrepreneur <entrepreneur84@bk.ru>
2026-02-15 14:39:37 +0700

    Add configuration for WhatsApp and payment methods

--- workers/config.js
+++ workers/config.js
@@ -1,1 +1,23 @@
 
+export const CONFIG = {
+  WHATSAPP: {
+    ADMIN_NUMBER: "6285175313909"
+  },
+
+  PAYMENT: {
+    SEABANK: {
+      BANK: "SeaBank / Bank Kesejahteraan Ekonomi",
+      ACCOUNT_NAME: "AHMAD BARIZI",
+      ACCOUNT_NUMBER: "901981495649",
+      BANK_CODE: "535",
+      SWIFT: "SSPIIDJA"
+    },
+    JAGO: {
+      BANK: "Bank Jago",
+      ACCOUNT_NAME: "AHMAD BARIZI",
+      ACCOUNT_NUMBER: "109896731184"
+    },
+    BTC_WALLET: "ISI_WALLET_BTC",
+    ETH_WALLET: "ISI_WALLET_ETH"
+  }
+};
