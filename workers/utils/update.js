1025ce6f4d3162966c6b49499bbedc12027acfa5
Entrepreneur <entrepreneur84@bk.ru>
2026-02-15 14:40:37 +0700

    Implement autoUpdate function in update.js
    Add autoUpdate function to fetch and update latest data

--- workers/utils/update.js
+++ workers/utils/update.js
@@ -1,1 +1,8 @@
-
+export async function autoUpdate(env, url){
+  // bisa fetch versi terbaru & replace data
+  try{
+    const res = await fetch(url)
+    const jsonData = await res.json()
+    await env.CONFIG.put("latest", JSON.stringify(jsonData))
+  }catch(e){ console.error(e) }
+}
